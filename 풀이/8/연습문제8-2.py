9d0206570eb864571839a280e05f7b9375ad2c56
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2020-09-24 20:57:48 +0900

    Update 연습문제8-2.py

--- 풀이/8/연습문제8-2.py
+++ 풀이/8/연습문제8-2.py
@@ -1,11 +1,11 @@
 def select_sort(a):
     n = len(a)
     for i in range(0, n - 1):
-        min = i
+        max = i
         for j in range(i + 1, n):
-            if a[j] > a[min]:
-                min = j
-        a[i], a[min] = a[min], a[i]
+            if a[j] > a[max]:
+                max = j
+        a[i], a[max] = a[max], a[i]
 
 
 d = [2, 4, 5, 1, 3]
